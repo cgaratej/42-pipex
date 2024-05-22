5520e1c14faa3c7081f6db73b53b35308eae74ef
Camilo Andres Garatejo Moreno <cgaratej@car10s1.42barcelona.com>
2024-05-22 15:59:19 +0200

    add

---
+++ src_bonus/main.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgaratej <cgaratej@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 13:02:18 by cgaratej          #+#    #+#             */
+/*   Updated: 2024/05/22 12:42:41 by cgaratej         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//ps lf || ps lp id
+//kill -9
+#include "../includes/pipex.h"
+
+static void	son(char **argv, int *fd, char **env);
+static void	father(char **argv, int *fd, char **env);
+static void	exec_cmd(char *cmd, char **env);
+
+int	main(int argc, char **argv, char **env)
+{
+	int		fd[2];
+	pid_t	pid;
+	int		status;
+
+	if (argc != 5)
+		print_error("./pipex file1 cmd cmd file2\n", 0, 2);
+	if (pipe(fd) == -1)
+		print_error("error in pipe", 1, 2);
+	pid = fork();
+	if (pid == -1)
+		print_error("error in fork", 1, 2);
+	if (!pid)
+		son(argv, fd, env);
+	wait(&status);
+	if (pid)
+		father(argv, fd, env);
+	return (0);
+}
+
+static void	son(char **argv, int *fd, char **env)
+{
+	int	fd_s;
+
+	close(fd[0]);
+	fd_s = open_file(argv[1], 1);
+	if (dup2(fd_s, STDIN_FILENO) == -1)
+		print_error("error failed to redirect stdout", 1, 2);
+	if (dup2(fd[1], STDOUT_FILENO) == -1)
+		print_error("error failed to redirect stdin", 1, 2);
+	close(fd_s);
+	exec_cmd(argv[2], env);
+}
+
+static void	father(char **argv, int *fd, char **env)
+{
+	int	fd_p;
+
+	close(fd[1]);
+	fd_p = open_file(argv[4], 0);
+	if (dup2(fd_p, STDOUT_FILENO) == -1)
+		print_error("error failed to redirect stdout", 1, 2);
+	if (dup2(fd[0], STDIN_FILENO) == -1)
+		print_error("error failed to redirect stdin", 1, 2);
+	close(fd_p);
+	exec_cmd(argv[3], env);
+}
+
+static void	exec_cmd(char *cmd, char **env)
+{
+	char	**cmd_l;
+	char	*path;
+
+	cmd_l = ft_split(cmd, ' ');
+	if (!cmd_l)
+		print_error("command not found\n", 0, 127);
+	path = get_path(cmd_l[0], env);
+	if (execve(path, cmd_l, env) == -1)
+	{
+		free_paths(cmd_l); 
+		print_error("command not found\n", 0, 127);
+	}
+	free_paths(cmd_l);
+}
---
+++ src_bonus/utils.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgaratej <cgaratej@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 17:00:50 by cgaratej          #+#    #+#             */
+/*   Updated: 2024/05/22 11:07:26 by cgaratej         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+void	print_error(char *str, int per, int ex)
+{
+	if (per)
+	{
+		perror(str);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		ft_putstr_fd(str, 2);
+		exit(ex);
+	}
+}
+
+int	open_file(char *file, int infile)
+{
+	int	fd;
+
+	if (infile)
+	{
+		fd = open(file, O_RDONLY, 0644);
+		if (access(file, R_OK))
+		{
+			if (fd == -1)
+				print_error("infile", 1, 1);
+			print_error("permission denied\n", 0, 126);
+		}
+	}
+	else
+	{
+		fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (access(file, W_OK))
+		{
+			if (fd == -1)
+				print_error("outfile", 1, 1);
+			print_error("permission denied\n", 0, 126);
+		}
+	}
+	return (fd);
+}
+
+void	free_paths(char **path)
+{
+	int	i;
+
+	i = 0;
+
+	while (path[i])
+	{
+		free(path[i]);
+		i++;
+	}
+	free(path);
+
+}
+
+static char	*get_env(char **env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (!ft_strncmp(env[i], "PATH=", 5))
+			return (env[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*get_path(char *cmd, char **env)
+{
+	int		i;
+	char	**path_list;
+	char	*path;
+	char	*path_final;
+
+	i = 0;
+	path_list = ft_split(get_env(env), ':');
+	if (!path_list)
+		print_error("command not found\n", 0, 127);
+	while (path_list[i])
+	{
+		path = ft_strjoin(path_list[i], "/");
+		if (!path)
+			print_error("command not found\n", 0, 127);
+		path_final = ft_strjoin(path, cmd);
+		if (!path_final)
+			print_error("command not found\n", 0, 127);
+		free(path);
+		if (!access(path_final, X_OK | F_OK))
+			return (path_final);
+		free(path_final);
+		i++;
+	}
+	free_paths(path_list);
+	return (cmd);
+}
